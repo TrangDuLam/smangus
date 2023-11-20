4e79edf3cc9de8ae0f6e9a7ccc747627778363aa
TrangDuLam <andyhardaway07@gmail.com>
2023-11-20 20:10:31 +0800

    master

--- atayal_extractor.py
+++ atayal_extractor.py
@@ -13,14 +13,8 @@ import os
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Wav2Vec2CTCTokenizer
 from utils import *
 
-'''
-processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-xlsr-53-espeak-cv-ft")
-tokenizer = Wav2Vec2CTCTokenizer.from_pretrained("facebook/wav2vec2-xlsr-53-espeak-cv-ft")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-xlsr-53-espeak-cv-ft")
-'''
-
 def extract_ppg(input_wav: pathlib.Path, output_dir: str, backend : str = 'xlsr-53') :
-    
+
     processor, _, model = load_hugginface_model(backend)
     
     waveform, sample_rate = torchaudio.load(input_wav)
