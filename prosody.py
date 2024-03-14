51230f5bd6437acb384e452013b9d5ccc4f9cdec
TrangDuLam <andyhardaway07@gmail.com>
2024-03-14 11:32:50 +0800

    master

--- prosody.py
+++ prosody.py
@@ -3,3 +3,25 @@ import numpy as np
 import wave
 import matplotlib.pyplot as plt
 import soundfile as sf
+
+import typing
+import numpy.typing as npt
+
+def pitch_contour_pruned(filename: str, to_prune : bool = False) :
+    '''
+    Extract pitch contour from a wav file using WORLD Vocoder.
+    filename: str, the name of the wav file.
+    to_prune: bool, determine whether to remove the voiceless frames.
+    '''
+    
+    y, fs = sf.read(filename)
+    
+    assert fs == 16000, "Sampling rate must be 16000"
+    assert len(y.shape) == 1, "Only mono audio files are supported"
+    
+    f0, _, _ = pw.wav2world(y, fs)
+    
+    if to_prune:
+        f0 = f0[f0 != 0]
+    
+    return f0
